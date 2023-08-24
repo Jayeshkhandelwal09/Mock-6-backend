96287e253fe128ceb99c0439a1ba6fbbad173f6f
Jayeshkhandelwal09 <khandelwaljayesh10@gmail.com>
2023-08-24 19:48:04 +0530

    backend -6

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const connection = mongoose.connect("mongodb+srv://Jayesh:khandelwal@cluster0.lizzrd7.mongodb.net/Masai_blog?retryWrites=true&w=majority")
+const connection = mongoose.connect("mongodb+srv://Jayesh:khandelwal@cluster0.lizzrd7.mongodb.net/Masai_blog?retryWrites=true&w=majority" ,{useNewUrlParser: true, useUnifiedTopology: true})
 
 module.exports ={
     connection
